5319254011efd170a21cb40dcb42ed28f41ecff7
Frank Zhao <frank.zhao.main@gmail.com>
2012-11-04 22:20:03 -0500

    introduced pocketwatch.h

--- code/PocketWatch.c
+++ code/PocketWatch.c
@@ -11,7 +11,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#include "pins.h"
+#include "pocketwatch.h"
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -20,42 +20,6 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <avr/eeprom.h>
 #include <util/delay.h>
 
-// pin definitions
-#define PORTx_BUTTON1 PORTB
-#define DDRx_BUTTON1 DDRB
-#define PINx_BUTTON1 PINB
-#define PIN_BUTTON1 3
-#define PORTx_BUTTON2 PORTB
-#define DDRx_BUTTON2 DDRB
-#define PINx_BUTTON2 PINB
-#define PIN_BUTTON2 1
-#define PORTx_LOWBATTDET PORTA
-#define DDRx_LOWBATTDET DDRA
-#define PINx_LOWBATTDET PINA
-#define PIN_LOWBATTDET 3
-#define PORTx_BUZZER PORTA
-#define DDRx_BUZZER DDRA
-#define PINx_BUZZER PINA
-#define PIN_BUZZER 4
-#define PORTx_MOTOR PORTA
-#define DDRx_MOTOR DDRA
-#define PINx_MOTOR PINA
-#define PIN_MOTOR 6
-#define PORTx_RINGS PORTF
-#define DDRx_RINGS DDRF
-#define PIN_RING_HOUR 2
-#define PIN_RING_MINUTE_EVEN 5
-#define PIN_RING_MINUTE_ODD 4
-#define PIN_RING_SECOND_EVEN 1
-#define PIN_RING_SECOND_ODD 0
-
-// useful macros
-#define button1_is_down() bit_is_clear(PINx_BUTTON1, PIN_BUTTON1)
-#define button1_is_up() bit_is_set(PINx_BUTTON1, PIN_BUTTON1)
-#define button2_is_down() bit_is_clear(PINx_BUTTON2, PIN_BUTTON2)
-#define button2_is_up() bit_is_set(PINx_BUTTON2, PIN_BUTTON2)
-#define battery_is_low() bit_is_clear(PORTA, 3)
-#define debounce() _delay_us(100)
 volatile uint8_t cur_hour;
 volatile uint8_t cur_minute;
 volatile uint8_t cur_second;
